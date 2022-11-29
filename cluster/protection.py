8287fc1bdfa2983a874d8b4f25f7a35683753981
subha_py <subha.py@gmail.com>
2022-11-29 19:45:33 +0530

    protection job creation with mp

--- cluster/protection.py
+++ cluster/protection.py
@@ -9,7 +9,7 @@ import os
 from itertools import cycle
 import random
 from cluster.connection import setup_cluster_automation_variables_in_environment, get_client_cycle
-
+from multiprocessing import Pool, cpu_count
 from s3.utils.bucket import (
     get_buckets_from_prefix,
 )
@@ -41,54 +41,63 @@ def get_policy_info(policy_name):
     if response.status_code == 200:
         return response.json()[0]
 
-# todo replace policy id with name
-def create_protection(bucket_list, policy_id="3928833934229838:1668777508219:118686", effective_now=True):
+def process_protection_request(bucket_name, ip, policy_id, effective_now):
     headers = {'Content-Type': "application/json", 'accept': "application/json"}
     headers['Authorization'] = "bearer {}".format(os.environ.get('accessToken'))
+    bucket_info = get_bucket_info(bucket_name)
+    if bucket_info.get('viewProtection'):
+        print("bucket: {} is already protected".format(bucket_name))
+        return
+    data = {
+        "name": "s3-test-{}".format(bucket_info['name']),
+        "environment": "kView",
+        "policyId": policy_id,
+        "viewBoxId": bucket_info['viewBoxId'],
+        "parentSourceId": 1,
+        "sourceIds": [
+            bucket_info['viewId']
+        ],
+        "timezone": "Asia/Calcutta"
+    }
+    response = requests.request("POST",
+                                "https://{}/irisservices/api/v1/public/protectionJobs".format(ip),
+                                verify=False,
+                                headers=headers, json=data)
+    if response.status_code == 201:
+        response_data = response.json()
+        if effective_now:
+            id = response_data["uid"]["id"]
+            data = {
+                "runType": "kRegular"
+            }
+            response = requests.request("POST",
+                                        "https://{}/irisservices/api/v1/public/protectionJobs/run/{}".format(
+                                            ip, id), verify=False,
+                                        headers=headers, json=data)
+            if response.status_code == 204:
+                print("Successfully protected bucket - {}".format(bucket_name))
+                return "Success"
+            else:
+                return "Failed"
+        else:
+            return response_data
+
+# todo replace policy id with name
+def create_protection(bucket_list, policy_id="3928833934229838:1668777508219:118686", effective_now=True):
+
     ips = os.environ.get("node_ips").split(",")
     ip_cycle = cycle(ips)
-    # policy_info = get_policy_info(policy_name)
+    # pool = Pool(processes=cpu_count() - 1)
     for bucket_name in bucket_list:
-        bucket_info = get_bucket_info(bucket_name)
-        if bucket_info.get('viewProtection'):
-            print("bucket: {} is already protected".format(bucket_name))
-            continue
-        data = {
-            "name": "s3-test-{}".format(bucket_info['name']),
-            "environment": "kView",
-            "policyId": policy_id,
-            "viewBoxId": bucket_info['viewBoxId'],
-            "parentSourceId": 1,
-            "sourceIds": [
-                bucket_info['viewId']
-            ],
-            "timezone": "Asia/Calcutta"
-        }
-        response = requests.request("POST",
-                                    "https://{}/irisservices/api/v1/public/protectionJobs".format(next(ip_cycle)),
-                                    verify=False,
-                                    headers=headers, json=data)
-        if response.status_code == 201:
-            response_data = response.json()
-            if effective_now:
-                id = response_data["uid"]["id"]
-                data = {
-                    "runType": "kRegular"
-                }
-                response = requests.request("POST",
-                                            "https://{}/irisservices/api/v1/public/protectionJobs/run/{}".format(
-                                            next(ip_cycle), id), verify=False,
-                                            headers=headers, json=data)
-                if response.status_code == 204:
-                    return "Success"
-                else:
-                    return "Failed"
-            else:
-                return response_data
+        arg = (bucket_name, next(ip_cycle), policy_id, effective_now)
+        process_protection_request(*arg)
+        # pool.apply_async(process_protection_request, args=arg)
+    # pool.close()
+    # pool.join()
+
 if __name__ == '__main__':
     setup_cluster_automation_variables_in_environment(cluster_ip="10.2.195.75")
     client_cycle = get_client_cycle()
 
-    # buckets = get_buckets_from_prefix(next(client_cycle), prefix="LCMTestBucket_Random_16")
-    res = get_bucket_info('LCMTestBucket_Random_15')
-    print(res)
+    buckets = get_buckets_from_prefix(next(client_cycle), prefix="LCMTestBucket_Object_0")
+    res = create_protection(buckets)
